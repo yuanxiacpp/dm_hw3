829df99b17d85b6455626f1b72a835faeaa29172
Yuan Xia <yuan.xia@yale.edu>
2013-12-06 20:53:55 -0500

    sync code, finish processing actualArray

--- project3.c
+++ project3.c
@@ -7,6 +7,11 @@
 #include "heap.h"
 
 int BIG_NUMBER = 100000;
+int PROCESSED = 0;
+int START = 1;
+int END = 2;
+int FATHER = 3;
+int CHILDREN = 4;
 
 void printMatrix(int *a, int row, int col) {
   int i, j;
@@ -36,21 +41,12 @@ int compareInt(const void *a, const void *b) {
   const int *right = b;
   return *left - *right;
 }
-void addNewEntry(int **actualArray, int row) {
-  return;
-}
 
-void seek(double *a, int n, int k, int *iz) {
-  int i, j;
-  int *permutation = (int*)malloc(n*sizeof(int));
-  for (i = 0; i < n; i++)
-    permutation[i] = i; 
-  
-  double boxArray[BIG_NUMBER][4];
-  int actualArray[BIG_NUMBER][8];
-  int idx = 0;
-  actualArray[idx] = {0, 0, n-1, -1, 0, 0, 0, 0};
-  
+void swapInt(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+  return;
 }
 
 void find_k_smallest_naive(double *d_matrix, int n, int k, int row, int *iz) {
@@ -95,6 +91,94 @@ void seek_naive(double *a, int n, int k, int *iz) {
   for (i = 0; i < n; i++) 
     find_k_smallest_naive(d_matrix, n, k, i, iz);
 }
+void addNewBoxEntry(int **boxArray, int row, double x1, double x2, double y1, double y2) {
+  boxArray[row][0] = x1;
+  boxArray[row][1] = x2;
+  boxArray[row][2] = y1;
+  boxArray[row][3] = y2;
+  return;
+}
+
+
+void addNewActualEntry(int **actualArray, int row, int start, int end, int father) {
+  int i;
+  for (i = 0; i < 8; i++)
+    actualArray[row][i] = 0;
+  actualArray[row][START] = start;
+  actualArray[row][END] = end;
+  actualArray[row][FATHER] = father;
+  return;
+}
+
+int isInBox(double *a, int n, int i, double **boxArray, int row) {
+  int x = a[i];
+  int y = a[n+i];
+  if (x < boxArray[row][0] || x > boxArray[row][1])
+    return 0;
+  if (y < boxArray[row][2] || y > boxArray[row][3])
+    return 0;
+  return 1;
+}
+
+void quadSplit(int **actualArray, double **boxArray, int *permutation, int current, int next, double *a, int n) {
+  int head = actualArray[current][START];
+  int tail = actualArray[current][END];
+  double x1 = boxArray[current][0];
+  double x2 = boxArray[current][1];
+  double y1 = boxArray[current][2];
+  double y2 = boxArray[current][3];
+  
+
+  addNewBoxEntry(boxArray, next, x1, (x1+x2)/2, (y1+y2)/2, y2);
+  addNewBoxEntry(boxArray, next+1, (x1+x2)/2, x2, (y1+y2)/2, y2);
+  addNewBoxEntry(boxArray, next+2, x1, (x1+x2)/2, y1, (y1+y2)/2);
+  addNewBoxEntry(boxArray, next+3, (x1+x2)/2, x2, y1, (y1+y2)/2);
+  
+  int i;
+  int count = 0;
+  while (count < 4) {
+    int s = head;
+    int e = tail;
+    while (s <= e) {
+      if (isInBox(a, n, permutation[s], boxArray, next+count)) 
+	s++;
+      else 
+        swapInt(permutation[s], permutation[e--]);
+    }
+    addNewActualEntry(actualArray, next+count, head, s-1, current);
+    actualArray[current][CHILDREN+count] = next+count;
+    count++;
+    head = s;
+  }
+  return;
+}
+
+void seek(double *a, int n, int k, int *iz) {
+  int i, j;
+  int *permutation = (int*)malloc(n*sizeof(int));
+  for (i = 0; i < n; i++)
+    permutation[i] = i; 
+  
+  double boxArray[BIG_NUMBER][4];
+  int actualArray[BIG_NUMBER][8];
+ 
+  addNewActualEntry(actualArray, 0, 0, n-1, -1);
+  addNewBoxEntry(boxArray, 0, 0.0, 1.0, 0.0, 1.0);
+  
+  int current = 0;
+  int next = 1;
+  while (current < next) {
+    //count how many points are there in the section
+    if (actualArray[current][START]-actualArray[current][END] > k) {
+      quadSplit(actualArray, boxArray, permutation, current, next, a, n);
+      next += 4;
+    }
+    actualArray[current][0] = 1;
+    current++;
+  }
+ 
+}
+
 
 void problem(int n, int k) {
   double *a = (double*)malloc(n*2*sizeof(double));
